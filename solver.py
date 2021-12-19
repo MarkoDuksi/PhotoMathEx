47adbeedaf9b6dad776b74ed0c1acadb25dce8b5
Marko Dukši <dookshi@gmail.com>
2021-12-19 02:10:57 +0100

    Finish documentation, add tests

--- solver.py
+++ solver.py
@@ -4,90 +4,95 @@
 import re
 import operator
 
-
 operators = {
     '+': operator.add,
-    '-': operator.add,    # this is not a mistake
+    '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
 }
 
 op_precedences = [
-    # extracts content between innermost parentheses
-    r'\(([^()]+)\)',
-    # extracts higher order operator (* or /) and its adjoining operands
-    r'(-?\d+(?:\.\d+)?)\s*([*/])\s*(-)?\s*(\d+(?:\.\d+)?)',
-    # extracts lower order operator (+ or -) and its adjoining operands
-    r'(-?\d+(?:\.\d+)?)\s*([+-])\s*(-)?\s*(\d+(?:\.\d+)?)',
+    # extract content between innermost parentheses - highest precedence
+    r'\(\s*([^()]+)\)',
+
+    # extract higher precedence operator (* or /) and its adjoining operands
+    r'(\d+(?:\.\d+)?)\s*([*/])\s*(-?\s*\d+(?:\.\d+)?)',
+
+    # extract lower precedence operator (+ or -) and its adjoining operands
+    r'(-?\s*\d+(?:\.\d+)?)\s*([+-])\s*(-?\s*\d+(?:\.\d+)?)',
 ]
 
 
-def operate(match):
-    # evaluating parenthesized expressions if present
-    if len(match.groups()) == 1:
-        subresult = evaluate(match.group(1), noparentheses=True)
-        return str(subresult)
+def validate(expression):
+    """Expression validator and reformatter
 
-    # after exhausting all parenthesized subexpressions
-    left, right = match.group(1, 4)
-    operator = operators[match.group(2)]
+    Receive a candidate expression as a string of space-separated
+    characters. Valid characters are all of "0123456789.+-*/() ".
+    Basic checking is performed as well as reformatting. If valid,
+    return a reformatted expression as a string, otherwise return
+    None.
+    """
 
-    # the two-signs construct allows for more flexible expressions
-    sign1 = match.group(2)
-    if sign1 == '-':
-        sign1 = -1
-    else:
-        sign1 = 1
-    sign2 = match.group(3)
-    if sign2 == '-':
-        sign2 = -1
-    else:
-        sign2 = 1
+    # check for characters not in a valid set, not just for the
+    # purpose of validating an expression but also to guard against
+    # malicious code injections
+    if re.search(r'[^\d.+\-*/() ]', expression):
+        return None
 
-    result = operator(float(left), sign1 * sign2 * float(right))
-    if result == round(result):
-        result = int(result)
+    # disallow expressions starting with any of "+", "*" or "/"
+    if re.search(r'^\s*[+*/]', expression):
+        return None
 
-    return str(result)
+    # check parentheses for mismatch
+    if not check_parentheses(expression):
+        return None
+
+    # manage space around negation sign - subtraction operator
+    processed = re.sub(r'(?<=\()-\s+([\d\(])', r' -\1', expression)
+    processed = re.sub(r'(?<=\(\s)-\s+([\d\(])', r'-\1', processed)
+
+    # join digits separated only by space
+    processed = re.sub(r'(?<=\d)\s+(?=\d)', r'', processed)
+
+    # join operators separated only by space
+    processed = re.sub(r'(?<=[-+*/])\s+(?=[-+*/])', r'', processed)
+
+    # disallow neighboring operators
+    if re.search(r'[-+*/]{2,}', processed):
+        return None
+
+    # disallow "+","*" or "/" between two identical parentheses
+    if re.search(r'([\(\)])\s*[+*/]\s*\1', processed):
+        return None
+
+    # disallow "-" between two closing parentheses
+    if re.search(r'\)\s*-\s*\)', processed):
+        return None
+
+    # enclose everything in parentheses for convenience
+    processed = re.sub(r'^(.+)$', r'( \1 )', processed)
+
+    return processed
 
 
-def evaluate(expression, noparentheses=False):
-    # colapse even number of consecutive negative signs
-    reduced = re.sub(r'(?:--)+', ' ', expression)
-
-    # evaluate in order of operator precedences
-    noparentheses = int(noparentheses)
-    for order in range(noparentheses, 3):
-        # initial update is None
-        updated = None
-        while True:
-            updated = re.sub(op_precedences[order], operate, reduced, count=noparentheses)
-            if (updated == reduced):    # if done with this level of operator precedence
-                break    # proceed to a lower level
-            reduced = updated    # otherwise repeat
-
-    # if the result is a whole number, cast it to int
-    result = float(reduced)
-    if result == round(result):
-        result = int(result)
-
-    return result
-
-
-# helper func for `validate`
+# helper function for `validate`
 def check_parentheses(expression):
-    """Check parentheses for missmatch
+    """Check parentheses for mismatch
 
     Receive a candidate expression as a string and return True if all
-    parentheses are properly closed. Nesting is allowed. If an error is
+    parentheses are properly matched. Nesting is allowed. If an error is
     detected, return False. If no parentheses are present, return True.
     """
 
-    if re.search(r'\(\s*\)', expression):
+    # disallow paired parentheses containing only space and/or
+    # operator(s) or nothing at all
+    if re.search(r'\([\s+*/-]*\)', expression):
         return False
 
+    # construct a list of parentheses to check
     parentheses = re.findall(r'[()]', expression)
 
+    #  parity
     count = 0
     counter = {
         '(': 1,
@@ -96,87 +101,255 @@ def check_parentheses(expression):
 
     for parenthesis in parentheses:
         count += counter[parenthesis]
-        if count < 0:    # if before any position there are more ")" than "("
+
+        # if left of any point there is more closed than opened parentheses
+        if count < 0:
             return False
+
+    # if after counting through the entire expression
+    # there is more opened than closed parentheses (count > 0)
     if count:
-        return False     # if by the end there are more "(" than ")"
-    return True          # if by the end the `count` stays or returns to zero
+        return False
+
+    return True
 
 
-def validate(expression):
-    """Validator/reformatter for expressions
+def evaluate(expression, noparentheses=False):
+    """Evaluator functionality of the solver module
 
-    Receive a candidate expression as a string of space-separated
-    characters. Valid characters are all of: 0123456789.+-*/().
-    Basic checking is performed as well as reformatting. If valid,
-    return a reformatted expression as a string, otherwise return
-    None.
+    Receive a validated and reformatted expression from `validate`. If
+    it is None, return None. If it passed validation, evaluate it in
+    order of operator precedence, second only to order enforced by
+    parentheses. If the result is a whole number cast it to int. Return the result.
     """
 
-    # check parentheses for missmatch
-    if not check_parentheses(expression):
-        return None
+    # skip the zero precedence if no parentheses are present
+    noparentheses = int(noparentheses)
 
-    # redistribute spaces around negation sign
-    processed = re.sub(r'\(\s*-\s+([\d\(])', r'( -\1', expression)
-    # join digits separated only by spaces
-    processed = re.sub(r'(?<=\d)\s+(?=\d)', r'', processed)
-    # join operators separated only by spaces
-    processed = re.sub(r'(?<=[-+*/])\s+(?=[-+*/])', r'', processed)
-    # enclose in parentheses for convenience
-    processed = re.sub(r'^(.+)$', r'( \1 )', processed)
+    # evaluate expression in order of operator precedences
+    for order in range(noparentheses, 3):
+        # "initial" update is not a thing, therefore:
+        updated = None
 
-    if re.search(r'[-+*/]{2,}', processed):    # neighbouring operators
-        return None
+        # for the current level of precedence
+        while True:
+            # call `operate` on an appropriate re.match object
+            updated = re.sub(op_precedences[order], operate, expression, count=noparentheses)
 
-    return processed
+            # cancel out even number of consecutive negative signs
+            updated = re.sub(r'(?:--)+', '+', updated)
+
+            #
+            updated = re.sub(r'^\+', '0+', updated)
+
+            # when done with this level of operator precedence
+            if (updated == expression):
+                # proceed to a lower level (higher `order` count)
+                break
+
+            # otherwise repeat the while loop
+            # reducing the expression stepwise from left to right
+            expression = updated
+
+    # if the result is a whole number, cast it to int
+    result = float(expression)
+    if result == round(result):
+        result = int(result)
+
+    return result
 
 
-def main():
+def operate(match):
+    """Perform a single calculation on a group of strings
+
+    Receive a re.match object from `evaluate`. The object contains
+    subgroups of strings captured by regex groups.
+
+    In case the match contains a single subgroup, it is guaranteed not
+    to contain parentheses. Send the string to `evaluate` for next
+    level processing.
+
+    In case the match contains 3 subgroups, it is guaranteed to be a
+    match of a binary operation. Convert the two operand strings
+    (from subgroups 1 and 3) to floats. Resolve the operator string
+    (from subgroup 2) to the correct `operator` method. Call the
+    `operator` method on the operands in the correct order. If the
+    result is a whole number cast it to int. Return the result.
+    """
+
+    # evaluate subexpression from inside parentheses, if present
+    if len(match.groups()) == 1:
+        subresult = evaluate(match.group(1).strip(), noparentheses=True)
+        return str(subresult)
+
+    # otherwise treat as a simple binary expression
+    left, right = match.group(1, 3)
+
+    # remove space between a "-" sign and operand, if present
+    left = re.sub(r'-\s+', '-', left)
+    right = re.sub(r'-\s+', '-', right)
+
+    # define the correct operator method to use
+    operator_ = operators[match.group(2)]
+
+    # perform the calculation
+    result = operator_(float(left), float(right))
+
+    if result == round(result):
+        result = int(result)
+
+    return str(result)
+
+
+def run_tests():
     tests = {
-        '18/(6*-3)+(3*2)': None,
-        '-18/(6*-3)+(3*2)': None,
-        '2 * 6 / 3 + (3 * 1))': None,
-        '(2 * 6 / 3 + (3 * 1)': None,
-        '2 * 6 / 3 + )(3 * 1)(': None,
-        '3*- ( 6)': None,
-        '-18/ -(6* -3)+(-3*2)': None,
-        '()': None,
-        '4 + ( ) 3': None,
-        '2 * 6 / 3 + (3 * 1)': 7,
-        '2 * (6 / 3) + (3 * 1)': 7,
-        '18 / 6 * 3 + (3 * 2)': 15,
-        '18 / (6 * 3) + (3 * 2)': 7,
-        '-18 / (-(6 * (-3))) + (-3 * 2)': -7,
-        '(2 * 6 / 3 + (3 * 1))': 7,
-        '((2 * 6 / 3 + (3 * 1)))': 7,
-        '(((2 * 6) / 3 + (((3) * (1)))))': 7,
-        '2 + 1 - 2 + 3 - 10': -6,
-        '2 + (1 - 2) + 3 - 10': -6,
-        '2 + (1 - 2)': 1,
-        '2.5 * 4 / (1/3) + (0.1 * 20)': 32,
-        '2.5 * 4 / (1/3) + (0.15555 * 20)': 33.111,
-        '0 + 1 + 2 + 3 - 4 - 5 * ( 6 + 7 - ( 8 + 9) )': 22,
-        '3 * 8 - 4 * 2 / 4 * ( - ( 3 + 3 ) )': 36,
-        '3 * (- 6)': -18,
-        '4 + 6 - ( 10 - 3 ) * 2 / 8 * 3': 4.75,
-        '4 0 0 + 6 - ( 1 0 0 - 3 ) * 2 / 8 * 3': 333.25,
+        '( 3 % 2 )': None,
+        '3 + 2 f': None,
+        '( 3 $ + 2 $)': None,
+        '! ( 3 + 2 )': None,
+        '( 3 + 2 ) =': None,
+        '( 3 + 2 ) :': None,
+        '~ 1': None,
+        '0 ^ 1': None,
+        '3 + 2 \\': None,
+        '3 + 2 \0': None,
+        '3 + 2 \n': None,
+        '3 + 2 \r': None,
+        '3 + 2 \t': None,
+        '3 + 2 F': None,
+        '- 1 8 / ( 6 * - 3 ) + ( 3 * 2 )': None,
+        '2 * 6 / 3 + ( 3 * 1 ) )': None,
+        '( 2 * 6 / 3 + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ) ( 3 * 1 ) (': None,
+        '3 * - ( 6 )': None,
+        '- 1 8 / - ( 6 * - 3 ) + ( - 3 * 2 )': None,
+        '( )': None,
+        '2 * 6 / 3 + ( + ( - 3 ) ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( * ( - 3 ) ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( / ( - 3 ) ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( ( - 3 ) * ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( ( - 3 ) / ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( ( - 3 ) - ) + ( 3 * 1 )': None,
+        '2 * 6 / 3 + ( ( - 3 ) + ) + ( 3 * 1 )': None,
+        '+ 2 + 2': None,
+        '* 2 + 2': None,
+        '/ 2 + 2': None,
+        '4 + ( + ) 3': None,
+        '4 + ( - ) 3': None,
+        '4 + ( * ) 3': None,
+        '4 + ( / ) 3': None,
+        '2 * 6 / 3 + ( 3 * 1 )': '?',
+        '2 * 6 / 3 + ( - ( - ( - ( - ( - 3 ) ) ) ) ) + ( 3 * 1 )': '?',
+        '2 * 6 / 3 + ( - ( - ( - 3 ) ) ) + ( 3 * 1 )': '?',
+        '2 * 6 / 3 + ( ( - ( - 3 ) ) ) + ( 3 * 1 )': '?',
+        '2 * ( 6 / 3 ) + ( 3 * 1 )': '?',
+        '1 8 / 6 * 3 + ( 3 * 2 )': '?',
+        '1 8 / ( 6 * 3 ) + ( 3 * 2 )': '?',
+        '- 1 8 / ( - ( 6 * ( - 3 ) ) ) + ( - 3 * 2 )': '?',
+        '( 2 * 6 / 3 + ( 3 * 1 ) )': '?',
+        '( ( 2 * 6 / 3 + ( 3 * 1 ) ) )': '?',
+        '( ( ( 2 * 6 ) / 3 + ( ( ( 3 ) * ( 1 ) ) ) ) )': '?',
+        '2 + 1 - 2 + 3 - 1 0': '?',
+        '2 + ( 1 - 2 ) + 3 - 1 0': '?',
+        '2 + ( 1 - 2 )': '?',
+        '- 2 + 2': '?',
+        '2.5 * 4 / ( 1 / 3) + ( 0.1 * 2 0 )': '?',
+        '2.5 * 4 / ( 1 / 3 ) + ( 0.15555 * 2 0 )': '?',
+        '0 + 1 + 2 + 3 - 4 - 5 * ( 6 + 7 - ( 8 + 9 ) )': '?',
+        '3 * 8 - 4 * 2 / 4 + ( - ( 3 + 3 ) )': '?',
+        '3 * ( - 6 )': '?',
+        '4 + 6 - ( 1 0 - 3 ) * 2 / 8 * 3': '?',
+        '4 0 0 + 6 - ( 1 0 0 - 3 ) * 2 / 8 * 3': '?',
+        '( 3 - 4 ) * ( 5 - 6 ) / ( 5 - 6 )': '?',
+        '3 - 4 * 2 ': '?',
         # there are some limitation to representing large numbers
-        # due to double-precission floating point type specs (IEEE 754):
-        # '12345678901234567890': 12345678901234567890,   # AssertionError
-        # '12345678901234*1000000 + 567890': 12345678901234567890,   # AssertionError
+        # due to double-precision floating point type specs (IEEE 754):
+        # '12345678901234567890': 12345678901234567890,   # test fails
+        # '12345678901234*1000000 + 567890': 12345678901234567890,   # test fails
     }
 
-    for k, value in tests.items():
-        validated = validate(k)
+    tests_condensed = {
+        '(3%2)': None,
+        '3+2f': None,
+        '(3$+2$)': None,
+        '!(3+2)': None,
+        '(3+2)=': None,
+        '(3+2):': None,
+        '~1': None,
+        '0^1': None,
+        '3+2\\': None,
+        '3+2\0': None,
+        '3+2\n': None,
+        '3+2\r': None,
+        '3+2\t': None,
+        '3+2F': None,
+        '-18/(6*-3)+(3*2)': None,
+        '2*6/3+(3*1))': None,
+        '(2*6/3+(3*1)': None,
+        '2*6/3+)(3*1)(': None,
+        '3*-(6)': None,
+        '-18/-(6*-3)+(-3*2)': None,
+        '()': None,
+        '2*6/3+(+(-3))+(3*1)': None,
+        '2*6/3+(*(-3))+(3*1)': None,
+        '2*6/3+(/(-3))+(3*1)': None,
+        '2*6/3+((-3)*)+(3*1)': None,
+        '2*6/3+((-3)/)+(3*1)': None,
+        '2*6/3+((-3)-)+(3*1)': None,
+        '2*6/3+((-3)+)+(3*1)': None,
+        '+2+2': None,
+        '*2+2': None,
+        '/2+2': None,
+        '4+(+)3': None,
+        '4+(-)3': None,
+        '4+(*)3': None,
+        '4+(/)3': None,
+        '2*6/3+(3*1)': '?',
+        '2*6/3+(-(-(-(-(-3)))))+(3*1)': '?',
+        '2*6/3+(-(-(-3)))+(3*1)': '?',
+        '2*6/3+((-(-3)))+(3*1)': '?',
+        '2*(6/3)+(3*1)': '?',
+        '18/6*3+(3*2)': '?',
+        '18/(6*3)+(3*2)': '?',
+        '-18/(-(6*(-3)))+(-3*2)': '?',
+        '(2*6/3+(3*1))': '?',
+        '((2*6/3+(3*1)))': '?',
+        '(((2*6)/3+(((3)*(1)))))': '?',
+        '2+1-2+3-10': '?',
+        '2+(1-2)+3-10': '?',
+        '2+(1-2)': '?',
+        '-2+2': '?',
+        '2.5*4/(1/3)+(0.1*20)': '?',
+        '2.5*4/(1/3)+(0.15555*20)': '?',
+        '0+1+2+3-4-5*(6+7-(8+9))': '?',
+        '3*8-4*2/4+(-(3+3))': '?',
+        '3*(-6)': '?',
+        '4+6-(10-3)*2/8*3': '?',
+        '400+6-(100-3)*2/8*3': '?',
+        '(3-4)*(5-6)/(5-6)': '?',
+        '3-4*2': '?',
+        # there are some limitation to representing large numbers
+        # due to double-precision floating point type specs (IEEE 754):
+        # '12345678901234567890': 12345678901234567890,   # test fails
+        # '12345678901234*1000000 + 567890': 12345678901234567890,   # test fails
+    }
+
+    passfail = {
+        True: 'passed',
+        False: 'failed'
+    }
+
+    for expression, value in {**tests, **tests_condensed}.items():
+        validated = validate(expression)
         if validated:
-            result = evaluate(validated)
-            print(f'{result == value} -> valid expression: {k} = {result}')
-            assert result == value
+            solver_result = evaluate(validated)
+            eval_result = eval(validated)
+            print(f'{passfail[solver_result == eval_result]} -> valid expression: {expression} = {solver_result}')
+            assert solver_result == eval_result
         else:
-            print(f'{validated is value} -> invalid expression: {k}')
+            print(f'{passfail[validated is value]} -> invalid expression: {expression}')
             assert validated is value
 
 
 if __name__ == '__main__':
-    main()
+    run_tests()
