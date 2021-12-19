cae1c7626890642e9efd35f20a58207cc4c87b75
Marko Dukši <dookshi@gmail.com>
2021-12-19 02:27:07 +0100

    Fill in the details in documentation
    Move the dataset building functions into a separate module.

--- extractor.py
+++ extractor.py
@@ -3,16 +3,10 @@
 
 import numpy as np
 
-# ------- Start of imports for dataset building helper functions -------
-# import os
-# import re
-# import hashlib
-# import pandas as pd
-# ------- End of imports for dataset building helper functions -------
-
 
 # set reasonable minimum dimensions for an input image array
 # in practice the range is in thousands of pixels
+# if providing already cropped images, decrease both accordingly
 MIN_IMG_WIDTH = 500
 MIN_IMG_HEIGHT = 300
 
@@ -20,36 +14,41 @@ MIN_IMG_HEIGHT = 300
 # in practice this results in good amount of despecling
 MIN_CHAR_WIDTH = 15     # lowest allowable width for "(" and ")"
 MIN_CHAR_HEIGHT = 8     # lowest allowable height for "-"
-MIN_LINE_HEIGHT = 50    # lowest allowable height for all but "+", "-" and "x"
+MIN_LINE_HEIGHT = 50    # lowest allowable height for the whole line
 
 # used for calculating custom threshold value
 # expressed in units of sigma over the histogram of uint8 values
 WHITE_SPREAD = 2
 
-# used to mark specles which score very close to this max
-# derived from sum of uint8 values over the framed "character" output
-MAX_INK_VALUE = 102_000
-
 
 def desaturate(img):
-    """Custom desaturate RGB image
+    """Desaturate an RGB or BGR image
 
-    Desaturate by keeping the value of the darkest pixel in any of the
-    red, green and blue channel representations. White background and
-    black ink are not affected but even bright ink of commonly used
-    colors (ie blue and red) in the same image is desaturated to near
-    black to facilitate thresholding.
+    Receive an RGB or BGR image as a 3D uint8 numpy array. Custom
+    desaturate by keeping only the value of the darkest pixel in any of
+    the channels. White background and black ink are therefore not
+    affected but even very bright ink of commonly used colors (namely
+    blue and red but also valid for green, cyan, magenta and even yellow)
+    in the same image is desaturated to near black to facilitate
+    thresholding. Return a single-channel image as a 2D uint8 numpy array.
+
+    Note: Specifically for text detection against a white-balanced
+    background this should be the preferred desaturating method followed
+    by custom thresholding even (especially) if the intent is to use the
+    thresholded result by OpenCV contour detection or similar algorithms.
     """
 
     return img.min(axis=2)
 
 
 def autostretch(img, black=None, white=None):
-    """Stretch dinamic range of an img
+    """Stretch the dynamic range of a single-channel image
 
-    By default it auto-adjusts black and white levels to 0 and 255
-    respectiverly, stretcing the values between. Custom levels can be
-    provided by arguments to `black` and/or `white` parameters.
+    Receive a single-channel image as a 2D uint8 numpy array. By default,
+    auto-adjust black and white levels to 0 and 255, respectively,
+    stretching linearly the values in between. Custom levels can be
+    provided by arguments to `black` and/or `white` parameters. Return a new
+    image in original format.
     """
 
     img_out = img.astype(float)
@@ -66,29 +65,33 @@ def autostretch(img, black=None, white=None):
 
 
 def autothresh(img):
-    """Threshold an img to b/w
+    """Threshold a single-channel image to black and white
 
-     Apply custom parameters based on assumptions of what a histogram
-     of a blank paper with some ink on it should look like. Make it
-     indifferent of underexposure which is to be commonly expected.
+     Receive a single-channel image as a 2D uint8 numpy array. Apply
+     custom parameters based on assumptions of what a histogram of a
+     blank paper with some ink on it should look like. Make it
+     indifferent to underexposure which is to be commonly expected.
+     Return a new image in original format.
 
-     TBD: Adaptive version to accommodate for uneven lighting and/or
-     shadows.
-     TBD: Despecling could be done here.
+     Note: This is a very, very basic implementation but even this
+     slight tailoring to text detection purpose yields better results
+     than OpenCV implementations.
+
+     TBD: Accommodate for uneven lighting and/or shadows.
     """
 
     # autostretch it
     img_out = autostretch(img).astype(float)
 
     # median is a good approximation of the middle "background" value
-    # mode was also tested, the difference was negligible
+    # mode was also tested but the difference was negligible
     median = np.median(img_out)
 
     # std is an indication of unevenness of lighting over the paper
     # ink values are basically outliers
     std = img_out.std()
 
-    # instead of the classic IQR outlier detection:
+    # instead of the classic IQR outlier detection, a custom one:
     thresh = max(median - WHITE_SPREAD * std, median / 2)
 
     # apply the threshold
@@ -99,24 +102,17 @@ def autothresh(img):
 
 
 def get_mask(img, axis=None):
-    """Detect regions of interest
+    """Detect regions of interest in a thresholded image
 
-    Assuming white background, get a mask vector indicating
-    rows or columns (depending on the argument to `axis`)
-    that contain any number of non-white pixels. Return
-    a one-dimensional boolean numpy array.
+    Receive a thresholded single-channel image as a 2D uint8 numpy array.
+    Assuming white background, get a mask vector
+    indicating rows or columns (depending on the argument to `axis`)
+    that contain any number of non-white pixels. Return the mask vector
+    as a 1D boolean numpy array.
 
-    Note: Along with `split-mask` functionality, this kind of
-    implementation allows for creative recursive implementations
-    by spliting each roi further and further in alternating
-    row/column fashion until no more spliting can be done.
-
-    Note 2: The speed of the implementation allowed for quickly
-    processing a large batch of sheets of characters into
-    >100,000 characters size labeled training/testing datasets
-
-    TBD: Return the coordinates of the possition of the mask
-    within the image.
+    Note: The speed of the implementation allowed for quickly
+    processing sheets of characters into >120,000 separate
+    characters labeled for training/validating dataset.
     """
 
     if axis not in (0, 1):
@@ -126,19 +122,18 @@ def get_mask(img, axis=None):
 
 
 def split_mask(mask, super=False, minsize=1):
-    """
-    Split the possibly fragmented mask returned by get_mask
-    into descrete masks. Retrun a list of masks, each one representing
-    one continuous fragment from the original mask.
+    """Split a multi-roi mask into single-roi submasks
+
+    Receive a mask vector as returned by `get_mask`. Split the possibly
+    fragmented mask into discrete masks, each for a single roi. Return a
+    list of mask vectors in original format.
 
     Optionally, if `super` is set to True, return a list with a single
-    supermask indicating a continuous region of interst containing all
-    sub-regions. Such mask cannot be split any further.
-
-    TBD: Despecling could also be done here.
+    supermask vector indicating a continuous region of interest
+    containing all sub-regions. Such mask cannot be split any further.
     """
 
-    # extend by a single False to accomadate offset by 1
+    # extend by a single False to accommodate offset by 1
     mask1 = np.array([*mask, False], dtype=bool)
 
     # prepend by a single False to make it offset by 1
@@ -157,12 +152,13 @@ def split_mask(mask, super=False, minsize=1):
         for idx in range(0, len(transitions), 2):
             start_pos = transitions[idx]
             end_pos = transitions[idx + 1]
+            # ignore the subregions smaller than minsize
             if (end_pos - start_pos) < minsize:
                 continue
             submask = np.zeros(mask.shape)
             submask[start_pos:end_pos] = 1
             submasks.append(submask.astype(bool))
-    else:                 # from the range between the outhermost pair
+    else:                 # from the range between the outermost pair
         start_pos = transitions[0]
         end_pos = transitions[-1]
         if (end_pos - start_pos) >= minsize:
@@ -174,10 +170,10 @@ def split_mask(mask, super=False, minsize=1):
 
 
 def sort_masks(masks, ascending=False):
-    """Sort masks by size
+    """Sort masks by roi size
 
-    Return a new list of masks sorted by size of their unmasked area.
-    Sort in descending order by default.
+    Receive a list of mask vectors as returned by `split_mask`. Return
+    a new list of mask vectors sorted by size of their respective roi.
     """
 
     reverse = not ascending
@@ -189,14 +185,11 @@ def sort_masks(masks, ascending=False):
 def extract_lines(img):
     """Line extractor
 
-    Receive an RGB image in a form of numpy array and return vertically
-    cropped blocks of content as a list of numpy arrays.
-
-    TBD: Return a corresponding list of height coordinates for each
-         cropped area from the original image.
+    Receive an RGB or BGR image as a 3D uint8 numpy array. Return
+    top/bottom-cropped blocks of content as a list of single-channel
+    black and white images in a 2D uint8 numpy array format.
     """
 
-    # TBD: write an input validaton function to dry this block out
     if not isinstance(img, np.ndarray):
         raise TypeError('not a valid numpy array')
     elif (img.ndim != 3) or (img.shape[2] != 3):
@@ -222,28 +215,24 @@ def extract_lines(img):
 
 
 def extract_chars(line):
-    """Char dextractor
+    """Character extractor
 
-    Receive a single numpy array from a list returned by `extract_lines`
-    and return horizontally cropped blocks of content as a list of numpy
-    arrays.
+    Receive a single image from a list returned by `extract_lines`.
+    Return all-around cropped blocks of content as a list of images in
+    original format, albeit of expectedly different shape.
 
-    TBD: Return a list of coords where each block was detected.
-    Note: Ask for specification of "coordinates", ie:
-          - rectangular bounding box corners, if so, which two?
-          - center of mass?
-          - ...
+    Left out: Return a list of coordinates where each block was detected.
+    Note: ask for specifications of "coordinates", ie:
+          - rectangular bounding box corners?
+          - if so, which two corners?
+          - with respect to origin located where?
+          - how are the exes defined?
     """
 
-    # TBD: write an input validaton function to dry this block out
-    if not isinstance(line, np.ndarray):
-        raise TypeError('not a valid numpy array')
-    elif line.ndim != 2:
-        raise ValueError(f'the image must be a greyscale image')
-    elif line.shape[0] < MIN_LINE_HEIGHT or line.shape[1] < MIN_IMG_WIDTH:
+    if line.shape[0] < MIN_LINE_HEIGHT or line.shape[1] < MIN_IMG_WIDTH:
         raise ValueError(f'minimum line dimensions are {MIN_IMG_WIDTH} by {MIN_LINE_HEIGHT}')
 
-    # further subdivide the line into charater candidates
+    # further subdivide the line into character candidates
     hmask = get_mask(line, axis=0)
     charmasks = split_mask(hmask, minsize=MIN_CHAR_WIDTH)
 
@@ -254,61 +243,24 @@ def extract_chars(line):
     # and crop the white area around each
     chars = []
     for charmask in charmasks:
-        char = line[:, charmask]          # crop char left and right
+        # first crop left and right
+        char = line[:, charmask]
+
+        # then mask out empty space above and below
         char_vmask = get_mask(char, axis=1)
+
+        # in case the mask is vertically fragmented
         char_vmasks = split_mask(char_vmask, minsize=MIN_CHAR_HEIGHT)
         char_vmasks = sort_masks(char_vmasks)
 
-        # if fragmented, select the tallest fragment of char roi or
-        # alternatively, use `super=True` in the previous mask split
+        # select the tallest fragment in the roi or alternatively,
+        # use `super=True` in the previous mask split
         #
-        # in practice, 'super=True' misses an oportunity to despecle
-        # the roi which can confuse the classifier
+        # Note: In practice, 'super=True' missed the opportunity to
+        # despecle the roi and there was at least one instance where
+        # a single specle was able to confuse the classifier.
         char_vmask = char_vmasks[0]
-        char = char[char_vmask]    # crop char top and bottom
+        char = char[char_vmask]
         chars.append(char)
 
     return chars
-
-
-# ------- Start of dataset building helper functions -------
-# TBD: documentation
-# def get_filenames(folder, regex):
-#     pagename_re = re.compile(regex)
-#     pages = []
-#     page_IDs = []
-
-#     for filename in os.listdir(folder):
-#         if (match := pagename_re.match(filename)):
-#             page_IDs.append(match.group(1))
-#             pagefilename = os.path.join(folder, filename)
-#             pages.append(pagefilename)
-
-#     return page_IDs, pages
-
-
-# def get_img_sig(img):
-#     img_string = ' '.join(img.ravel().astype(str))
-#     img_string_hashed = hashlib.md5(img_string.encode('utf-8')).hexdigest()
-
-#     return img_string_hashed
-
-
-# def get_ink_fraction(img):
-#     return img.sum() / MAX_INK_VALUE
-
-
-# def get_outliers(df):
-#     idxs = []
-#     for scriptstyle in df.scriptstyle.unique():
-#         for label in df.label.unique():
-#             subdf = df.loc[(df.scriptstyle == scriptstyle) & (df.label == label)]
-#             Q1 = subdf.ink_fraction.quantile(0.25)
-#             Q3 = subdf.ink_fraction.quantile(0.75)
-#             IQR = Q3 - Q1
-#             low = Q1 - 1.5 * IQR
-#             high = Q3 + 1.5 * IQR
-#             idxs.extend(list(subdf.loc[(subdf.ink_fraction < low) | (subdf.ink_fraction > high)].index))
-
-#     return idxs
-# ------- End of dataset building helper functions -------
